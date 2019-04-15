259e4975e9285330294d037d9cfd098721e11be4
Michaël Zasso <targos@protonmail.com>
2019-04-15 08:32:26 +0200

    fix tests on node 6 and expand travis testing

--- src/__tests__/maybeToPrecision.js
+++ src/__tests__/maybeToPrecision.js
@@ -29,6 +29,6 @@ describe('maybeToPrecision', () => {
   it('wrong digit option', () => {
     expect(function () {
       maybeToPrecision(0, 0);
-    }).toThrow('toPrecision() argument must be between 1 and 100');
+    }).toThrow(/toPrecision\(\) argument must be between 1 and (100|21)/);
   });
 });
