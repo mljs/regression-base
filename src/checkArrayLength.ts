ad8add8384cd166ce6c20c9b48a0bdca39aa527c
Santi Miranda <santimir@protonmail.com>
2023-08-07 23:14:54 +0100

    chore: improve description for function

--- src/checkArrayLength.ts
+++ src/checkArrayLength.ts
@@ -2,8 +2,9 @@ import { isAnyArray } from 'is-any-array';
 
 /**
  * Check that x and y are arrays, and they are of the same length
- * @param x
- * @param y
+ * @param x - first item to check
+ * @param y - second item to check
+ * @throws {TypeError} if x or y are not arrays
  */
 export default function checkArraySize(x: unknown, y: unknown): void {
   if (!isAnyArray(x) || !isAnyArray(y)) {
